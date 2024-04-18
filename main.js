0203ff4e5de7d319b044790e9b20cf89db170c30
Daniela Porras <Daniela@192.168.1.2>
2024-04-18 11:35:34 -0300

    [FIX] se soluciona problema con el cubo2

--- main.js
+++ main.js
@@ -3,42 +3,43 @@ gsap.registerPlugin(ScrollTrigger);
 
 // #region ejemplo de rotacion cuadrado
 
-// gsap.to(".cubo-1", {
-//     scrollTrigger: {
-//         trigger:".cubo-1",
-//         pin: true, // pin the trigger element while active
-//     start: "top top", // when the top of the trigger hits the top of the viewport
-//     end: "+=500", // end after scrolling 500px beyond the start
-//     scrub: 1, 
-//     },
-//     x: 500,
-//     rotation: 360,
-//     duration:3,
-//   });
+gsap.to(".cubo-1", {
+    scrollTrigger: {
+        trigger:".cubo-1",
+        pin: true, // pin the trigger element while active
+    start: "top top", // when the top of the trigger hits the top of the viewport
+    end: "+=500", // end after scrolling 500px beyond the start
+    scrub: 1, 
+    },
+    x: 500,
+    rotation: 360,
+    duration:3,
+  });
 
-//   gsap.to(".cubo-2", {
-//     ScrollTrigger:{
-//         trigger:'.cubo-2',
-//         toggleActions: "play complete reverse reset"
-//     },
-//     duration:1,
-//     scale:2,
-//     x:400,
-//     delay: 1,
-//     borderRadius:'50%',
-//   });
+  gsap.to(".cubo-2", {
+    scrollTrigger: {
+        trigger:".cubo-2",
+        pin: true, // pin the trigger element while active
+    start: "top top", // when the top of the trigger hits the top of the viewport
+    end: "+=500", // end after scrolling 500px beyond the start
+    scrub: 1, 
+    },
+    x: 500,
+    rotation: 360,
+    duration:3,
+  });
 
-//   gsap.from(".cubo-3", {
-//     scrollTrigger: {
-//       trigger: ".cubo-3",
-//     toggleActions: "play complete reverse reset"
-//     },
-//     duration:1,
-//     scale:2,
-//     x:400,
-//     delay: 1,
-//     borderRadius:'50%', //TODO:Tambien puede ser escrito como  borderRadius:50
-//   });
+  gsap.from(".cubo-3", {
+    scrollTrigger: {
+      trigger: ".cubo-3",
+    toggleActions: "play complete reverse reset"
+    },
+    duration:1,
+    scale:2,
+    x:400,
+    delay: 1,
+    borderRadius:'50%', //TODO:Tambien puede ser escrito como  borderRadius:50
+  });
 
 // #region una especie de parallax que baja el texto con la section 
 
@@ -93,52 +94,52 @@ gsap.registerPlugin(ScrollTrigger);
 // });
 
 
-// #region Scrolling" Section Transitions
-let sections = gsap.utils.toArray("section"),
-    currentSection = sections[0],
-    isLastSection = false;
+// // #region Scrolling" Section Transitions
+// let sections = gsap.utils.toArray("section"),
+//     currentSection = sections[0],
+//     isLastSection = false;
 
-gsap.defaults({overwrite: 'auto', duration: 0.3});
+// gsap.defaults({overwrite: 'auto', duration: 0.3});
 
-// stretch out the body height according to however many sections there are. 
-gsap.set("body", {height: (sections.length * 100) + "%"});
+// // stretch out the body height according to however many sections there are. 
+// gsap.set("body", {height: (sections.length * 100) + "%"});
 
-// create a ScrollTrigger for each section
-sections.forEach((section, i) => {
-  ScrollTrigger.create({
-    // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
-    start: () => (i - 0.5) * innerHeight,
-    end: () => (i + 0.5) * innerHeight,
-    // when a new section activates (from either direction), set the section accordinglyl.
-    onToggle: self => {
-      self.isActive && setSection(section);
-      // Check if it's the last section
-      isLastSection = section === sections[sections.length - 1];
-    }
-  });
-});
+// // create a ScrollTrigger for each section
+// sections.forEach((section, i) => {
+//   ScrollTrigger.create({
+//     // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+//     start: () => (i - 0.5) * innerHeight,
+//     end: () => (i + 0.5) * innerHeight,
+//     // when a new section activates (from either direction), set the section accordinglyl.
+//     onToggle: self => {
+//       self.isActive && setSection(section);
+//       // Check if it's the last section
+//       isLastSection = section === sections[sections.length - 1];
+//     }
+//   });
+// });
 
-function setSection(newSection) {
-  if (newSection !== currentSection) {
-    gsap.to(currentSection, {scale: 0.8, autoAlpha: 0})
-    gsap.to(newSection, {scale: 1, autoAlpha: 1});
-    currentSection = newSection;
-  }
-}
+// function setSection(newSection) {
+//   if (newSection !== currentSection) {
+//     gsap.to(currentSection, {scale: 0.8, autoAlpha: 0})
+//     gsap.to(newSection, {scale: 1, autoAlpha: 1});
+//     currentSection = newSection;
+//   }
+// }
 
-// handles the infinite part, wrapping around at the end when scrolling down...
-ScrollTrigger.create({
-  start: 1,
-  end: () => ScrollTrigger.maxScroll(window) - 1,
-  onLeaveBack: self => {
-    // Stop scrolling infinitely if it's not the first section
-    if (!isLastSection && ScrollTrigger.maxScroll(window) === window.scrollY) {
-      self.scroll(2);
-    }
-  },
-  onLeave: self => {
-    if (!isLastSection) {
-      self.scroll(2);
-    }
-  }
-}).scroll(2);
+// // handles the infinite part, wrapping around at the end when scrolling down...
+// ScrollTrigger.create({
+//   start: 1,
+//   end: () => ScrollTrigger.maxScroll(window) - 1,
+//   onLeaveBack: self => {
+//     // Stop scrolling infinitely if it's not the first section
+//     if (!isLastSection && ScrollTrigger.maxScroll(window) === window.scrollY) {
+//       self.scroll(2);
+//     }
+//   },
+//   onLeave: self => {
+//     if (!isLastSection) {
+//       self.scroll(2);
+//     }
+//   }
+// }).scroll(2);
